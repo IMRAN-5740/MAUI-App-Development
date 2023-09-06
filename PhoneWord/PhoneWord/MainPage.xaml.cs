b590f80f439c1d0ec6f410d26a1296d3c034c390
Mohammad Abdullah Al Imran <imranbsmrstucse@gmail.com>
2023-09-06 22:56:36 +0600

    New Initialize

---
+++ PhoneWord/PhoneWord/MainPage.xaml.cs
@@ -0,0 +1,41 @@
+﻿namespace PhoneWord
+{
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        string translatedNumber;
+        private void OnTranslate(object sender, EventArgs e)
+        {
+            string enteredNumber = PhoneNumberText.Text;
+            translatedNumber = PhoneWord.PhonewordTranslator.ToNumber(enteredNumber);
+
+            if (!string.IsNullOrEmpty(translatedNumber))
+            {
+                CallButton.IsEnabled = true;
+                CallButton.Text = "Call " + translatedNumber;
+            }
+            else
+            {
+                CallButton.IsEnabled = false;
+                CallButton.Text = "Call";
+            }
+        }
+        async void OnCall(object sender, System.EventArgs e)
+        {
+            if (await this.DisplayAlert(
+                "Dial a Number",
+                "Would you like to call " + translatedNumber + "?",
+                "Yes",
+                "No"))
+            {
+                // TODO: dial the phone
+            }
+        }
+    }
+}
---
+++ SampleApp/SampleApp/MainPage.xaml.cs
@@ -0,0 +1,24 @@
+﻿namespace SampleApp
+{
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void OnCounterClicked(object sender, EventArgs e)
+        {
+            count+=10;
+
+            if (count == 1)
+                CounterBtn.Text = $"Clicked {count} times";
+            else
+                CounterBtn.Text = $"Congratulations ,I am a Dot Net Maui Developer{count} th";
+
+            SemanticScreenReader.Announce(CounterBtn.Text);
+        }
+    }
+}
