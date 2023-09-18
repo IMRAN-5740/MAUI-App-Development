588d08b29537391285b79848176835a75f4817f3
Mohammad Abdullah Al Imran <imranbsmrstucse@gmail.com>
2023-09-18 12:38:22 +0600

    New app Created

---
+++ Maui_Crud_SqLite/Maui_Crud_SqLite/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace Maui_Crud_SqLite
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ Maui_Crud_SqLite/Maui_Crud_SqLite/MainPage.xaml.cs
@@ -0,0 +1,13 @@
+﻿namespace Maui_Crud_SqLite
+{
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+    }
+}
