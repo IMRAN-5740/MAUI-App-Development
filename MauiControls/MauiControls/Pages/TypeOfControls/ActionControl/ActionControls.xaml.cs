a9790576de7025f5bf0e63fbe30253eb33ba6f19
Mohammad Abdullah Al Imran <imranbsmrstucse@gmail.com>
2023-10-04 21:29:07 +0600

    Swipe View and SearchBar

--- MauiControls/MauiControls/Pages/TypeOfControls/ActionControl/ActionControls.xaml.cs
+++ MauiControls/MauiControls/Pages/TypeOfControls/ActionControl/ActionControls.xaml.cs
@@ -24,4 +24,28 @@ public partial class ActionControls : ContentPage
         DisplayAlert("ReadLine Academy", $"You have search  {searchButton.Text}", "Ok", FlowDirection.LeftToRight);
 
     }
+
+    private void facebookSwipeItem_Invoked(object sender, EventArgs e)
+    {
+        DisplayAlert("ReadLine Academy", "You have Clicked  Facebook Link", "Ok", FlowDirection.LeftToRight);
+
+    }
+
+    private void instagramSwipeItem_Invoked(object sender, EventArgs e)
+    {
+        DisplayAlert("ReadLine Academy", "You have Clicked  Instagram Link", "Ok", FlowDirection.LeftToRight);
+
+    }
+
+    private void twitterSwipeItem_Invoked(object sender, EventArgs e)
+    {
+        DisplayAlert("ReadLine Academy", "You have Clicked  Twitter Link", "Ok", FlowDirection.LeftToRight);
+
+    }
+
+    private void gmailSwipeItem_Invoked(object sender, EventArgs e)
+    {
+        DisplayAlert("ReadLine Academy", "You have Clicked  Gmail Link", "Ok", FlowDirection.LeftToRight);
+
+    }
 }
