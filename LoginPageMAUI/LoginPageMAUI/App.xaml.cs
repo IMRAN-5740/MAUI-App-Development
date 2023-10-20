f1e516f5eb273b435e01c5536d27d73bc7231a95
Mohammad Abdullah Al Imran <imranbsmrstucse@gmail.com>
2023-10-20 12:34:58 +0600

    Log In Page Design

---
+++ LoginPageMAUI/LoginPageMAUI/App.xaml.cs
@@ -0,0 +1,27 @@
+﻿using LoginPageMAUI.Handlers;
+using Microsoft.Maui.Platform;
+
+namespace LoginPageMAUI
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+            //Border less entry
+            Microsoft.Maui.Handlers.EntryHandler.Mapper.AppendToMapping(nameof(BorderLessEntry), (handler, view) =>
+            {
+                if (view is BorderLessEntry)
+                {
+#if __ANDROID__
+                    handler.PlatformView.SetBackgroundColor(Colors.Transparent.ToPlatform());
+#elif __IOS__
+                handler.PlatformView.BorderStyle = UIKit.UITextBorderStyle.None;
+#endif
+                }
+            });
+           
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ LoginPageMAUI/LoginPageMAUI/obj/Debug/net7.0-android/android/src/mono/android/app/ApplicationRegistration.java
@@ -0,0 +1,12 @@
+package mono.android.app;
+
+public class ApplicationRegistration {
+
+	public static void registerApplications ()
+	{
+				// Application and Instrumentation ACWs must be registered first.
+		mono.android.Runtime.register ("LoginPageMAUI.MainApplication, LoginPageMAUI, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null", crc64759d730bb024d653.MainApplication.class, crc64759d730bb024d653.MainApplication.__md_methods);
+		mono.android.Runtime.register ("Microsoft.Maui.MauiApplication, Microsoft.Maui, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null", crc6488302ad6e9e4df1a.MauiApplication.class, crc6488302ad6e9e4df1a.MauiApplication.__md_methods);
+		
+	}
+}
